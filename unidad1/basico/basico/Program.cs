09d3cce600c750e6fba9b86ed629ee199b75ff77
EDUARDJ1001 <jeduard7030@gmail.com>
2023-01-27 19:01:05 -0600

    primer proyecto con tipos de datos.

---
+++ unidad1/basico/basico/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using basico.tipos_datos;
+
+namespace basico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA desde program");
+
+            Tipos01 t01 = new Tipos01();
+        }
+    }
+}
---
+++ unidad1/basico/basico/tipos-datos/Tipos01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using basico.tipos_datos;
+
+namespace basico.tipos_datos
+{
+    internal class Tipos01
+    {
+        public Tipos01() 
+        {
+            Console.WriteLine("HOLA desde tipos01");
+        }
+    }
+}
